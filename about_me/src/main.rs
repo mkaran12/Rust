c226c8364603a760d6ffaee8750b14480d670f53
mkaran12 <mosesjnr46466@gmail.com>
2025-10-16 15:31:38 +0300

    project

---
+++ about_me/src/main.rs
@@ -0,0 +1,6 @@
+//to compile a rust program you run cargo build
+fn main() {
+    println!("Hello, I am Moses Karani");
+    println!("Hello, I am Software Engineer");
+    println!("I love Coding");
+}
