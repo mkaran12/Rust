97a2c92ad4f97181c7112dec6c6a0981b52efd56
mkaran12 <mosesjnr46466@gmail.com>
2025-10-16 15:04:22 +0300

    rust

---
+++ hello_world/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // this is a comment
+    println!("Running algorithm...");
+    println!("Make profit..");
+    /* multi-line comment */
+}
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/mk/Code/Rust/hello_world/target/debug/hello_world: /home/mk/Code/Rust/hello_world/src/main.rs
