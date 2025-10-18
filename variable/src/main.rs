070d0e455a356bb82c7232d50b37105d3d292ce8
mkaran12 <mosesjnr46466@gmail.com>
2025-10-18 03:29:55 +0300

    immutable variable

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -1,10 +1,10 @@
 fn main(){
     let apples = 50;
     let oranges = 14 + 6;
-    let fruits = apples + oranges;
+    let _fruits = apples + oranges;
 
     println!("My garden");
-    println!("this years, garden has {fruits} fruits");
+    println!("this years, garden has {0} fruits, this was and amazing {1} fruits",apples,oranges);
 
 
 }
